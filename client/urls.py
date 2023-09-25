fd9d81380b13d213410ee9276d0fb9fee4b4040d
Vink77 <vink.av@mail.ru>
2023-09-25 17:35:11 +0700

    25.09

--- client/urls.py
+++ client/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from django.conf import settings
-from client.views import ClientListView
+from client.views import client_list
 from client.apps import ClientConfig
 
 
 app_name = ClientConfig.name
 
-urlpatterns = [path('', ClientListView.as_view(), name='list'),  # Домашняя страница
+#urlpatterns = [path('', ClientListView.as_view(), name='list'),  # Домашняя страница
+#               ]
+urlpatterns = [path('', client_list, name='client_list'),  # Домашняя страница
                ]
